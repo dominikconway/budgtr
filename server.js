c4e1494612ca704b32f80a78692cf902467ce8ab
Dominik Conway <dominik.conway@gmail.com>
2022-04-12 13:55:43 -0700

    adds stylesheets and formats show page and add item page

--- server.js
+++ server.js
@@ -2,27 +2,43 @@ const express = require('express')
 const app = express()
 const port = 3001
 const budget = require('./models/budget')
-app.use('/static', express.static('public'))
-
+const bodyParser = require('body-parser')
+/////////////////////////////////
+// adds middleware for serving static files
+///////////////////////////////////
+app.use('/public', express.static('public'))
+///////////////////////////////////
+/////////////middleware - used for post route - tells express to use middleware
+///////////////////////////////////
+app.use(express.urlencoded({ extended: false }));
+// app.use((req,res, next) => {
+//     console.log('i run for all routes')
+//     next()
+// })
+///////////////////////////////////
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
 // index route
 app.get('/budgets', (req,res) => {
-    res.send(budget)
+   res.render('index.ejs', {allBudgets: budget})
 })
-// new route
-app.get('/budgets/new', (req, res) => {
 
+app.get('/budgets/new', (req,res) => {
+    res.render('new.ejs')
 })
 // show route
 app.get('/budgets/:index', (req, res) => {
-    res.send(req.params.index)
+    res.render('show.ejs',{budgets: budget[req.params.index]})
 })
 
-app.post('/', (req, res) => {
+app.post('/budgets', (req, res) => {
+    //console.log(req.body);
+    budget.push(req.body)
+    res.redirect('/budgets')
+    
+  });
 
-})
 app.listen(port, () => {
 console.log(`We are listening on ${port}`)
 })
